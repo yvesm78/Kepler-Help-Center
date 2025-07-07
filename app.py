89e9b7929e13d7406b73e3362107a4bf4ee92188
yvesm78 <yvesm7818@gmail.com>
2025-07-07 15:00:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import google.generativeai as genai
 
 # --- CONFIGURE GOOGLE GEMINI ---
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["Yve_API_KEY"])
 model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
 
 # --- LOAD CSV DATA ---
